6bdaefbdc737c67e4d56ac15f38122bfe8b8d666
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-04-01 17:54:25 -0300

    menu e cores adicionadas

--- conversor/converte.sh
+++ conversor/converte.sh
@@ -2,8 +2,8 @@
 
 echo "Conversor de sass:"
 echo
-find ../codigo/css/ -name '*.scss' | sed 's/.*/sass &:&/' | sed 's/\.scss$/.css --style compressed/' |  sh
+find ../codigo/css/ -name '*.scss' | sed 's/.*/sass &:&/' | sed 's/\.scss$/.css/' |  sh
 find ../codigo/css/ -name '*.scss' | sed 's/.*/sass &:&/' | sed 's/\.scss$/.min.css --style compressed/' |  sh
 
-
+tar -czvf ../codigo/css/hcw-css.tar.gz ../codigo/css/
 
